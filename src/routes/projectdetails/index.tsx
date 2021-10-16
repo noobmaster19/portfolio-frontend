bc526f421da568776c3907170732f601539b7a2a
noobmaster19 <dionneo123@gmail.com>
2021-10-16 13:00:17 +0800

    'removed-conflicts'

--- src/routes/projectdetails/index.tsx
+++ src/routes/projectdetails/index.tsx
@@ -1,35 +1,5 @@
-<<<<<<< HEAD
-import { Heading, Text, HStack, GridItem, Grid, Container, Stack, Box } from '@chakra-ui/layout';
-import { useSelector } from 'react-redux';
-import { Button } from '@chakra-ui/button';
-import { Link } from 'preact-router';
-import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
-import Hero from '../../components/hero';
-import { PROJECT_DETAILS } from '../../config/projects';
-
-export default function ProjectDetail(props) {
-	const id = props.matches.id
-	const projectDetail = PROJECT_DETAILS.find((project)=>project.key == id)
-	return (
-		<Stack spacing="100px">
-			<Hero
-				title={projectDetail.title}
-				description={projectDetail.description}
-			/>
-			<Tabs variant="soft-rounded" colorScheme="green">
-				<Text>Hello</Text>
-			</Tabs>
-		</Stack>
-	);
-}
-=======
-import { Heading, Text, HStack, GridItem, Grid, Container, Stack, Box } from '@chakra-ui/layout';
-import { useSelector } from 'react-redux';
-import { Button } from '@chakra-ui/button';
-import { Link } from 'preact-router';
-import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
+import { Text, Stack } from '@chakra-ui/layout';
+import { Tabs } from "@chakra-ui/react"
 import Hero from '../../components/hero';
 import { PROJECT_DETAILS } from '../../config/projects';
 
@@ -49,4 +19,3 @@ export default function ProjectDetail(props) {
 		</Stack>
 	);
 }
->>>>>>> b240af70f580e996310a3b2bbfc299eab4984b04
